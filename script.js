6374bec60cb46e53999fb7a0c1feeea91d7f88af
praveenWebDesigner360 <m.praveenkumar.github@gmail.com>
2023-05-08 21:57:42 +0530

    Code practice

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 'use strict';
 
+const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const buttonsOpenModal = document.querySelectorAll('.show-modal');
-// console.log(buttonsOpenModal);
+const closeBtn = document.querySelector('.close-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-for (let i = 0; i < buttonsOpenModal.length; i++) {
-  buttonsOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
