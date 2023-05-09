51bb8ddadbbb8bf058e27518f1c1372a85722e20
praveenWebDesigner360 <m.praveenkumar.github@gmail.com>
2023-05-09 06:47:50 +0530

    keydown event

--- script.js
+++ script.js
@@ -5,18 +5,31 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-modal');
 
+const clickToOpen = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const clickToClose = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  showModal[i].addEventListener('click', clickToOpen);
 }
 
-closeBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+closeBtn.addEventListener('click', clickToClose);
+overlay.addEventListener('click', clickToClose);
+
+document.addEventListener('keydown', function (event) {
+  console.log(event.key);
+
+  if (event.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      console.log(!modal.classList.contains('hidden'));
+      clickToClose();
+    }
+  }
 });
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+
+console.log(!modal.classList.contains('hidden'));
